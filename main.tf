29471c647820158802497eb9035d5b63b4161ea4
Carlos Solis Salazar <carlos@soysoliscarlos.com>
2025-08-13 10:59:41 -0500

    Refactor key vault configuration: move access policy to a separate resource and add tags to key vault

--- main.tf
+++ main.tf
@@ -163,14 +163,15 @@ resource "azurerm_key_vault" "kv" {
   purge_protection_enabled   = var.kv_purge_protection
   soft_delete_retention_days = 7
 
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
-
-    secret_permissions = ["Get", "List", "Set", "Delete", "Recover", "Backup", "Restore"]
-  }
-
   tags = local.tags
+}
+
+resource "azurerm_key_vault_access_policy" "user" {
+  key_vault_id = azurerm_key_vault.kv.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
+
+  secret_permissions = ["Get", "List", "Set", "Delete", "Recover", "Backup", "Restore"]
 }
 
 resource "azurerm_key_vault_access_policy" "adf" {
